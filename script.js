55aa26b297b33209e8a9016e06a670416666a713
Eva Tan <111343276+evaaahq@users.noreply.github.com>
2022-11-22 17:43:59 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+var products = [{
+    "id": 0,
+    "title": "N/A",
+    "price": "N/A",
+    "description": "N/A",
+    "benefits": "N/A",
+    "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/No-Image-Placeholder.svg/1665px-No-Image-Placeholder.svg.png"
+}, {
+    "id": 1,
+    "title": "AutoCAD",
+    "price": ["Monthly: SGD 372", 
+                "Annually: SGD 2,968", 
+                "Every 3 years: SGD 8,456"],
+    "description": "AutoCAD is a commercial computer-aided design (CAD) and drafting software application. Developed and marketed by Autodesk.",
+    "benefits": ["HTML", "CSS", "JS"],
+    "image": "https://aux3.iconspalace.com/uploads/6399511681435985048.png"
+}, {
+    "id": 1,
+    "title": "BricsCAD",
+    "price": ["BricsCAD Lite: US$590", 
+                "BricsCAD Pro: US$1,150", 
+                "BricsCAD BIM: US$1,890",
+                "BricsCAD Mechanical: US$1,780"],
+    "description": "BricsCAD is a software application for computer-aided design (CAD), developed by Bricsys nv. In 2018, Bricsys nv was acquired in full by Hexagon AB of Sweden",
+    "benefits": ["DWG Compatible", 
+                "Instantly familiar to AutoCAD users", 
+                "Automatic conFiguration and customisation transfer",
+                "Intelligent CAD tools",
+                "2.7x faster in opening drawings (opening 500+ drawings)"],
+    "image": "https://logovectordl.com/wp-content/uploads/2020/03/bricscad-logo-vector.png"
+}]
